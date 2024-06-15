1afef88c8a0225856831b6b2148a50c7c978cda8
mkheck13 <mkheck13@gmail.com>
2024-06-15 08:59:32 -0700

    bool to string

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// this should have worked. I was missing a =. it works.
+function boolToWord (bool){
+    if (bool == true){
+        return 'Yes';
+    }else{
+        return 'No'
+    }
+}
